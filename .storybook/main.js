ece59d5adae9c47a37ed5cc4418c29941510315e
manaten <manaten@manaten.net>
2023-10-01 15:32:20 +0900

    feat: initial commit

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,19 +2,17 @@
 const path = require("path");
 
 module.exports = {
-  stories: [
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "storybook-addon-next",
-    '@chakra-ui/storybook-addon'
+    "@chakra-ui/storybook-addon",
   ],
   framework: "@storybook/react",
   core: {
-    "builder": "@storybook/builder-webpack5"
+    builder: "@storybook/builder-webpack5",
   },
   features: {
     emotionAlias: false,
@@ -22,26 +20,29 @@ module.exports = {
   typescript: {
     check: false,
     checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
+    reactDocgen: "react-docgen-typescript",
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
     },
   },
 
   webpackFinal: async (config = {}) => {
     config.devtool = "inline-source-map";
-    config.resolve.alias["@"] =  path.resolve(path.join(__dirname, "../src"));
+    config.resolve.alias["@"] = path.resolve(path.join(__dirname, "../src"));
     config.module.rules = [
-      ...config.module.rules.map(rule =>
-        rule.test && rule.test.test('a.svg') ? { ...rule, exclude: /\.svg$/ } : rule
+      ...config.module.rules.map((rule) =>
+        rule.test && rule.test.test("a.svg")
+          ? { ...rule, exclude: /\.svg$/ }
+          : rule,
       ),
       {
         test: /\.svg$/,
         issuer: /\.[jt]sx?$/,
-        use: '@svgr/webpack',
+        use: "@svgr/webpack",
       },
     ];
     return config;
   },
-}
+};
--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
-// @ts-check
-const path = require("path"); // eslint-disable-line @typescript-eslint/no-var-requires
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: "@svgr/webpack",
-    });
-
-    config.resolve.alias["@"] = path.resolve(path.join(__dirname, "src"));
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
